4a65c52f13e26052b83ea1fe229455e58d53dcf7
Manish Kumar <manish.kumar@didata.com.au>
2014-12-03 14:07:52 +1100

    nuget package mismatch

--- source/Testlib/resources/Resource.en-gb.js
+++ source/Testlib/resources/Resource.en-gb.js
@@ -1,3 +1,3 @@
 var Testlib = Testlib||{};
- Testlib.localization = Testlib.localization||{};
-Testlib.localization.Resource = {"String1":"test 34","String2":"test2","lcid":2057,"lang":"en-GB"};
+Testlib.localization = Testlib.localization||{};
+Testlib.localization.Resource = {"String1":"test 34","String2":"test2"};
--- source/Testlib/resources/Resource1.js
+++ source/Testlib/resources/Resource1.js
@@ -1,3 +1,3 @@
 var Testlib = Testlib||{};
- Testlib.localization = Testlib.localization||{};
-Testlib.localization.Resource1 = {"String1":"test 34","String2":"test2","lcid":0,"lang":""};
+Testlib.localization = Testlib.localization||{};
+Testlib.localization.Resource1 = {"String1":"test 34","String2":"test2"};
